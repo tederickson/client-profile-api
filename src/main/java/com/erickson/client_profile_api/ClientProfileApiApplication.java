3cb80b8fc2b81125b0551c7ace88bb4067dddd27
Ted Erickson <ted.erickson@comcast.net>
2026-01-27 07:03:50 -0700

    Spring Retry and recover

--- src/main/java/com/erickson/client_profile_api/ClientProfileApiApplication.java
+++ src/main/java/com/erickson/client_profile_api/ClientProfileApiApplication.java
@@ -2,14 +2,14 @@ package com.erickson.client_profile_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
+@EnableRetry
 public class ClientProfileApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ClientProfileApiApplication.class, args);
     }
-
 }
--- src/main/java/com/erickson/client_profile_api/service/UserProfileService.java
+++ src/main/java/com/erickson/client_profile_api/service/UserProfileService.java
@@ -10,8 +10,12 @@ import com.erickson.client_profile_api.mapper.UserProfileMapper;
 import com.erickson.client_profile_api.model.BeneficiaryDTO;
 import com.erickson.client_profile_api.model.UserProfileEntity;
 import com.erickson.client_profile_api.repository.UserProfileRepository;
+import jakarta.persistence.OptimisticLockException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -52,14 +56,32 @@ public class UserProfileService {
         return CompletableFuture.completedFuture(userProfileEntity);
     }
 
+    @Retryable(noRetryFor = UserProfileClientException.class, maxAttempts = 2, backoff = @Backoff(delay = 200))
     public UserProfileResponse createUserProfile(CreateUserProfileRequest createUserProfileRequest) {
+        log.info("Creating user profile {}", createUserProfileRequest);
         createUserProfileRequest.validate();
         createUserProfileRequest.addresses().forEach(Address::validate);
 
         UserProfileEntity userProfileEntity = UserProfileMapper.map(createUserProfileRequest);
 
+        // Replace the save with this line to verify the method is retried
+        // throw new OptimisticLockException("wahoo!");
         UserProfileEntity dbUserProfileEntity = userProfileRepository.save(userProfileEntity);
 
         return UserProfileMapper.map(dbUserProfileEntity);
+    }
+
+    // This method is called if createUserProfile() fails after 3 attempts
+    @Recover
+    public UserProfileResponse recover(OptimisticLockException e) {
+        log.error("Recovery logic executed.", e);
+        return new UserProfileResponse();
+    }
+
+    // This method is called as soon as the UserProfileClientException is thrown
+    @Recover
+    public UserProfileResponse handleUserProfileClientException(UserProfileClientException e) {
+        log.info("UserProfileClientException Recovery logic executed.");
+        throw e;
     }
 }
