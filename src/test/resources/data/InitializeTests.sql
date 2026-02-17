61dd28625f16e8808fd6912a31d554133d540507
Ted Erickson <ted.erickson@comcast.net>
2026-02-17 09:05:44 -0700

    Spring Retry and recover

--- src/test/resources/data/InitializeTests.sql
+++ src/test/resources/data/InitializeTests.sql
@@ -3,7 +3,7 @@ DELETE from user_profile;
 
 INSERT INTO user_profile (user_profile_id, version, first_name, last_name, date_of_birth)
 VALUES (1, 3, 'Richard', 'McDonnell', '1992-05-23'),
-       (2, 4, 'Alvaro', 'Gomez', '2002-06-23');
+       (2, 4, 'Derrick', 'Stanton', '2002-06-23');
 
 INSERT INTO address (address_id, version, user_profile_id, line1, city, state, zip_code, address_type)
 VALUES (1,
